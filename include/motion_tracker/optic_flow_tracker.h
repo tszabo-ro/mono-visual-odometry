39e2ca7bad826f588bd2aeaa25dd40086433cf92
tszabo <tamas@tszabo.ro>
2020-01-11 13:43:22 +0100

    Implement OpticFlowTracker
    Implement OpticFlowTracker, which is meant to track points in
    predefined regions of a grayscale Frame.

---
+++ include/motion_tracker/optic_flow_tracker.h
@@ -0,0 +1,38 @@
+#ifndef OpticFlowTracker_h
+#define OpticFlowTracker_h
+
+#include <motion_tracker/camera_frame.h>
+#include <motion_tracker/primitives.h>
+
+struct Projection
+{
+
+};
+
+struct OpticFlow
+{
+  OpticFlow(Point2f anchor, Vector2f flow): anchor(anchor), flow(flow) {}
+
+  Point2f anchor;
+  Vector2f flow;
+};
+
+class OpticFlowTracker
+{
+public:
+  OpticFlowTracker(const Frame& start_frame, Rect<unsigned int> roi, size_t num_points);
+  ~OpticFlowTracker();
+
+  [[nodiscard]] std::vector<OpticFlow> getFlowVectors(const Frame& frame, Projection proj = {});
+  [[nodiscard]] std::vector<Point2f> getTrackedPoints() const;
+
+private:
+  const Rect<unsigned int> roi;
+  const size_t num_tracked_points;
+
+  struct Internal;
+  std::unique_ptr<Internal> internal_;
+
+};
+
+#endif
---
+++ src/optic_flow_tracker.cpp
@@ -0,0 +1,118 @@
+#include <motion_tracker/optic_flow_tracker.h>
+#include <opencv2/imgproc.hpp>
+#include <opencv2/video/tracking.hpp>
+
+static double distanceSquared(const cv::Point2f& p1, const cv::Point2f& p2)
+{
+  double dx = p1.x - p2.x;
+  double dy = p1.y - p2.y;
+  return dx * dx + dy * dy;
+}
+
+static std::vector<cv::Point2f> findCorners(const cv::Mat& frame, unsigned int num_points, std::vector<cv::Point2f> points = std::vector<cv::Point2f>())
+{
+  if (points.size() >= num_points)
+  {
+    return points;
+  }
+
+  points.reserve(num_points);
+
+  const double min_distance = 20;
+  const double min_distance_sq = min_distance*min_distance;
+
+  std::vector<cv::Point2f> found_points;
+  goodFeaturesToTrack(frame, found_points, num_points * 2, 0.00001, min_distance);
+
+  for (const auto& pt : found_points)
+  {
+    if (points.size() >= num_points)
+    {
+      break;
+    }
+
+    bool point_ok = true;
+    for (const auto& f_pt : points) // This should be done based on a map rendering and not in n2 complexity!
+    {
+      if (distanceSquared(pt, f_pt) < min_distance_sq)
+      {
+        point_ok = false;
+        break;
+      }
+    }
+    if (point_ok)
+    {
+      points.emplace_back(pt);
+    }
+  }
+
+  return points;
+}
+
+struct OpticFlowTracker::Internal
+{
+  Frame last_frame;
+  std::vector<cv::Point2f> last_points;
+};
+
+OpticFlowTracker::OpticFlowTracker(const Frame& start_frame, Rect<unsigned int> roi, size_t num_points)
+  : roi(roi)
+    , num_tracked_points(num_points)
+    , internal_(std::make_unique<Internal>())
+{
+  internal_->last_frame = start_frame.crop(roi);
+  internal_->last_points = findCorners(internal_->last_frame.data(), num_tracked_points);
+}
+
+OpticFlowTracker::~OpticFlowTracker() = default;
+
+std::vector<OpticFlow> OpticFlowTracker::getFlowVectors(const Frame& frame, Projection proj)
+{
+  Frame cropped_frame = frame.crop(roi);
+
+  auto start_points = findCorners(cropped_frame.data(), num_tracked_points, internal_->last_points);
+
+  std::vector<uchar> status_values;
+  std::vector<float> err;
+
+  std::vector<cv::Point2f> tracked_points;
+
+  cv::TermCriteria criteria = cv::TermCriteria((cv::TermCriteria::COUNT) + (cv::TermCriteria::EPS), 20, 0.05);
+  cv::calcOpticalFlowPyrLK(internal_->last_frame.data(), cropped_frame.data(), start_points, tracked_points, status_values, err, cv::Size(30, 30), 1, criteria);
+
+  std::vector<cv::Point2f> found_points;
+  found_points.reserve(tracked_points.size());
+
+  std::vector<OpticFlow> optic_flow_vectors;
+  optic_flow_vectors.reserve(tracked_points.size());
+
+  for (size_t index = 0; index < status_values.size(); ++index)
+  {
+    if (status_values[index] == 1)
+    {
+      optic_flow_vectors.emplace_back(
+        Point2f(internal_->last_points[index].x, internal_->last_points[index].y),
+        Vector2f(tracked_points[index].x - internal_->last_points[index].x, tracked_points[index].y - internal_->last_points[index].y)
+      );
+      found_points.emplace_back(tracked_points[index]);
+    }
+  }
+
+  internal_->last_points.swap(found_points);
+  internal_->last_frame = std::move(cropped_frame);
+
+  return optic_flow_vectors;
+}
+
+std::vector<Point2f> OpticFlowTracker::getTrackedPoints() const
+{
+  std::vector<Point2f> points;
+  points.reserve(internal_->last_points.size());
+
+  for (const auto& pt : internal_->last_points)
+  {
+    points.emplace_back(pt.x, pt.y);
+  }
+
+  return points;
+}
