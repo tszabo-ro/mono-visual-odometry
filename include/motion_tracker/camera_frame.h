fd5690d9d4526f45cbf61caa5bf3d2e543debe5e
tszabo <tamas@tszabo.ro>
2020-01-11 13:42:24 +0100

    Add grayscale conversion to Frame

--- include/motion_tracker/camera_frame.h
+++ include/motion_tracker/camera_frame.h
@@ -7,11 +7,14 @@
 class Frame
 {
 public:
+  Frame() {}
   Frame(cv::Mat src): data_(std::move(src)) {}
 
+  [[nodiscard]] Frame toGray() const;
   [[nodiscard]] Frame crop(unsigned int start_x, unsigned int start_y, unsigned int end_x, unsigned int end_y) const;
 
-  [[nodiscard]] const cv::Mat& data() const { return data_; }
+  const cv::Mat& data() const { return data_; }
+  bool valid() const { return (data_.rows > 0) && (data_.cols > 0); }
 
 private:
   cv::Mat data_;
--- src/camera_frame.cpp
+++ src/camera_frame.cpp
@@ -1,7 +1,39 @@
 #include <motion_tracker/camera_frame.h>
+#include <opencv2/imgproc.hpp>
+
+Frame Frame::toGray() const
+{
+  if (!valid())
+  {
+    return Frame();
+  }
+
+  cv::Mat gray_frame;
+  cvtColor(data_, gray_frame, cv::COLOR_BGR2GRAY);
+
+  return Frame(std::move(gray_frame));
+}
 
 Frame Frame::crop(unsigned int start_x, unsigned int start_y, unsigned int end_x, unsigned int end_y) const
 {
+  if (!valid())
+  {
+    return Frame();
+  }
+
   cv::Rect roi(start_x, start_y, end_x - start_x, end_y - start_y);
+
+  bool x_ok = 0 <= roi.x;
+  bool y_ok = 0 <= roi.y;
+  bool w_pos = 0 <= roi.width;
+  bool h_pos = 0 <= roi.height;
+  bool w_ok = roi.x + roi.width <= data_.cols;
+  bool h_ok = roi.y + roi.height <= data_.rows;
+
+  if (!(x_ok && w_pos && w_ok && y_ok && h_pos && h_ok))
+  {
+    return Frame();
+  }
+
   return Frame(data_(roi));
 }
