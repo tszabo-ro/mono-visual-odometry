61f1d3ffb5119ffe552f45096afb0bc505d4088c
tszabo <tamas@tszabo.ro>
2020-01-12 20:29:22 +0100

    Implement turn rate and speed estimator
    Add the functions used to estimate the turn rate and the
    linear speed of the camera.

--- include/motion_tracker/motion_estimation.h
+++ include/motion_tracker/motion_estimation.h
@@ -4,7 +4,11 @@
 #include <motion_tracker/optic_flow.h>
 #include <motion_tracker/camera_config.h>
 #include <vector>
+#include <tuple>
 
+std::pair<double, std::vector<bool>> getTurnRateAndValidIndicatorFromFlow(const CameraConfig& params, const std::vector<OpticFlow>& flow);
 double getTurnRateFromFlow(const CameraConfig& params, const std::vector<OpticFlow>& flow);
 
+std::pair<double, std::vector<bool>> getSpeedAndValidIndicatorFromFlow(const CameraConfig& params, const std::vector<OpticFlow>& flow, double turn_rate);
+double getSpeedFromFlow(const CameraConfig& params, const std::vector<OpticFlow>& flow, double turn_rate);
 #endif
--- src/motion_estimation.cpp
+++ src/motion_estimation.cpp
@@ -1,7 +1,6 @@
 #include <motion_tracker/motion_estimation.h>
 
-
-double getTurnRateFromFlow(const CameraConfig& params, const std::vector<OpticFlow>& flow)
+std::pair<double, std::vector<bool>> getTurnRateAndValidIndicatorFromFlow(const CameraConfig& params, const std::vector<OpticFlow>& flow)
 {
 
   // As per the paper, the points are to be projected to a cylindrical frame and the flow (angular
@@ -18,6 +17,9 @@ double getTurnRateFromFlow(const CameraConfig& params, const std::vector<OpticFl
   std::vector<double> angular_flow;
   angular_flow.reserve(flow.size());
 
+  std::vector<bool> valid_flow;
+  valid_flow.reserve(flow.size());
+
   for (const auto& flow_element : flow)
   {
     double sin_start = (2.0*flow_element.start.x - params.img_width) / params.focal_length;
@@ -26,14 +28,83 @@ double getTurnRateFromFlow(const CameraConfig& params, const std::vector<OpticFl
     double angle_end = asin(sin_end);
 
     double f = angle_end - angle_start;
+    valid_flow.emplace_back(!std::isnan(f));
 
-    if (!std::isnan(f))
+    if (valid_flow.back())
     {
       angular_flow.emplace_back(f);
     }
   }
   std::sort(angular_flow.begin(), angular_flow.end());
 
+  if (angular_flow.size() < 2)
+  {
+    return std::make_pair<double, std::vector<bool>>(0.0, {false});
+  }
+
   double yaw_speed = angular_flow[angular_flow.size() / 2] / flow.front().dt;
-  return yaw_speed;
+  return std::make_pair(yaw_speed, valid_flow);
+}
+
+std::pair<double, std::vector<bool>> getSpeedAndValidIndicatorFromFlow(const CameraConfig& params, const std::vector<OpticFlow>& flow, double turn_rate)
+{
+  // The displacement of the camera can be calculated by projecting the flow onto the ground plane,
+  //  and removing the effects of the rotation from the observed flow. As per the paper, the magnitude
+  //  of the resulting flow vectors is an indication of the forward translation of the camera.
+
+  std::vector<double> linear_flow;
+  linear_flow.reserve(flow.size());
+
+  std::vector<bool> valid_flow;
+  valid_flow.reserve(flow.size());
+
+  for (const auto& flow_element : flow)
+  {
+    double s_elevation = atan2(2.0 * flow_element.start.y - params.img_height, params.focal_length);
+    double s_azimuth = atan2(2.0 * flow_element.start.x - params.img_width, params.focal_length);
+
+    double e_elevation = atan2(2.0 * flow_element.end.y - params.img_height, params.focal_length);
+    double e_azimuth = atan2(2.0 * flow_element.end.x - params.img_width, params.focal_length);
+
+    double s_depth = params.ground_height * cos(s_elevation) / sin(s_elevation + params.camera_pitch);
+    double e_depth = params.ground_height * cos(e_elevation) / sin(e_elevation + params.camera_pitch);
+
+    double s_x = s_depth * tan(s_azimuth);
+    double s_y = params.ground_height / tan(s_elevation + params.camera_pitch);
+
+    double e_x = e_depth * tan(e_azimuth);
+    double e_y = params.ground_height / tan(e_elevation + params.camera_pitch);
+
+    double dx = e_x - s_x - turn_rate * s_y;
+    double dy = e_y - s_y;
+
+    double speed_estimate = hypot(dx, dy);
+
+    valid_flow.emplace_back(!std::isnan(speed_estimate));
+
+    if (valid_flow.back())
+    {
+      linear_flow.emplace_back(speed_estimate);
+    }
+  }
+  std::sort(linear_flow.begin(), linear_flow.end());
+
+  if (linear_flow.size() < 2)
+  {
+    return std::make_pair<double, std::vector<bool>>(0.0, {false});
+  }
+  double linear_speed = linear_flow[linear_flow.size() / 2] / flow.front().dt;
+  return std::make_pair(linear_speed, valid_flow);
+}
+
+double getTurnRateFromFlow(const CameraConfig& params, const std::vector<OpticFlow>& flow)
+{
+  auto [turn_rate, valid_indicator] = getTurnRateAndValidIndicatorFromFlow(params, flow);
+  return turn_rate;
+}
+
+double getSpeedFromFlow(const CameraConfig& params, const std::vector<OpticFlow>& flow, double turn_rate)
+{
+  auto [displacement, valid_indicator] = getSpeedAndValidIndicatorFromFlow(params, flow, turn_rate);
+  return displacement;
 }
